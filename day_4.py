d4004b33bd0235a2fd72b57ad80a8ae8a83c963e
Neil <31366098+neilneil2000@users.noreply.github.com>
2022-08-16 18:43:05 +0100

    Day 4 Part 2 Complete

--- day_4.py
+++ day_4.py
@@ -21,6 +21,19 @@ def main():
     )
     print(f"Hash Output is: {hash_output.hexdigest()}")
 
+    while True:
+        suffix_number += 1
+        hash_input = puzzle_input + bytes(str(suffix_number), "UTF-8")
+        hash_output = md5(hash_input)
+        if hash_output.hexdigest()[:6] == "000000":
+            break
+
+    print(f"Matching Hash Found for {puzzle_input}")
+    print(
+        f"{suffix_number} is first number to result in 6 digest 0 prefix in hash digest"
+    )
+    print(f"Hash Output is: {hash_output.hexdigest()}")
+
 
 if __name__ == "__main__":
     main()
