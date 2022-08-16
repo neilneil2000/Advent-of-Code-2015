0ff40ba111db456d0fc12ba48517b78e7157552c
Neil <31366098+neilneil2000@users.noreply.github.com>
2022-08-16 18:41:23 +0100

    Day 4 Part 1 Complete

---
+++ day_4.py
@@ -0,0 +1,26 @@
+from hashlib import md5
+
+
+def main():
+    """Main function"""
+    puzzle_input = b"ckczppom"
+    suffix_number = 0
+
+    print(f"Finding hash for {puzzle_input}")
+
+    while True:
+        suffix_number += 1
+        hash_input = puzzle_input + bytes(str(suffix_number), "UTF-8")
+        hash_output = md5(hash_input)
+        if hash_output.hexdigest()[:5] == "00000":
+            break
+
+    print(f"Matching Hash Found for {puzzle_input}")
+    print(
+        f"{suffix_number} is first number to result in 5 digest 0 prefix in hash digest"
+    )
+    print(f"Hash Output is: {hash_output.hexdigest()}")
+
+
+if __name__ == "__main__":
+    main()
